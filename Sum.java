b9ff0e0f932954f199e7cff71d8cce6c653b99d2
shenweitc <97611442+shenweitc@users.noreply.github.com>
2022-09-02 20:21:17 +0800

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package Sun;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Sum="+sumMethod(54,43));
+    }
+
+    public static int sumMethod(int a,int b) {
+       return a+b;
+    }
+}
